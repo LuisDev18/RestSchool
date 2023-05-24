c3ddf7600dbd031efa728e910e32442065b1e2cd
LuisDev18 <brian@orbita.pe>
2023-05-24 13:29:25 -0500

    agregando dependencias en el pom.xml

---
+++ src/main/java/utp/edu/pe/ApiRestSchool/UtpRestApplication.java
@@ -0,0 +1,13 @@
+package utp.edu.pe.ApiRestSchool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UtpRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UtpRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/utp/edu/pe/ApiRestSchool/UtpRestApplicationTests.java
@@ -0,0 +1,13 @@
+package utp.edu.pe.ApiRestSchool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UtpRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
