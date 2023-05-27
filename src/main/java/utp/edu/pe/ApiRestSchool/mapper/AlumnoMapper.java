0ad3af67a4c68d52d87f4f525fa1faba3f99bbe6
LuisDev18 <brian@orbita.pe>
2023-05-27 10:23:51 -0500

    Creacion de dtos y mapper - validaciones

--- src/main/java/utp/edu/pe/ApiRestSchool/mapper/AlumnoMapper.java
+++ src/main/java/utp/edu/pe/ApiRestSchool/mapper/AlumnoMapper.java
@@ -1,7 +1,16 @@
 package utp.edu.pe.ApiRestSchool.mapper;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+import utp.edu.pe.ApiRestSchool.dto.AlumnoDto;
+import utp.edu.pe.ApiRestSchool.entity.Alumno;
 
 @Mapper
 public interface AlumnoMapper {
+    AlumnoMapper MAPPER = Mappers.getMapper(AlumnoMapper.class);
+
+    AlumnoDto mappToDto(Alumno alumno);
+
+    Alumno mappToEntity(AlumnoDto alumnoDto);
+
 }
