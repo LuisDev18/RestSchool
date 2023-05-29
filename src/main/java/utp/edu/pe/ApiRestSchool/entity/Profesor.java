dc300fa26e8c6fd38df9c924e2596d1c77c13add
zzTonyz <134666924+zzTonyz@users.noreply.github.com>
2023-05-28 19:54:22 -0500

    Update Profesor.java

--- src/main/java/utp/edu/pe/ApiRestSchool/entity/Profesor.java
+++ src/main/java/utp/edu/pe/ApiRestSchool/entity/Profesor.java
@@ -1,4 +1,36 @@
 package utp.edu.pe.ApiRestSchool.entity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
+import lombok.Data;
 
+@Entity
+@Data
 public class Profesor {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    
+    private String nombre;
+
+    
+    private String apellidoPaterno;
+
+    
+    private String apellidoMaterno;
+
+    
+    private String dni;
+
+    
+    private String celular;
+
+   
+    private String email;
+
+    
+    private String curso;
 }
