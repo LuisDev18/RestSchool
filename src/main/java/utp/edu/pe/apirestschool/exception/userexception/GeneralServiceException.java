973dba5f3ea5b209afc57af50892a4dcafd451cb
LuisDev18 <brian@orbita.pe>
2023-06-30 13:29:43 -0500

    identacion en global excepcion

--- src/main/java/utp/edu/pe/apirestschool/exception/userexception/GeneralServiceException.java
+++ src/main/java/utp/edu/pe/apirestschool/exception/userexception/GeneralServiceException.java
@@ -6,25 +6,25 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 @ResponseStatus(code = HttpStatus.INTERNAL_SERVER_ERROR)
 public class GeneralServiceException extends RuntimeException {
 
-    public GeneralServiceException() {
-      super();
-    }
+  public GeneralServiceException() {
+    super();
+  }
 
-    public GeneralServiceException(String message, Throwable cause, boolean enableSuppression,
+  public GeneralServiceException(String message, Throwable cause, boolean enableSuppression,
                                    boolean writableStackTrace) {
-      super(message, cause, enableSuppression, writableStackTrace);
-    }
+    super(message, cause, enableSuppression, writableStackTrace);
+  }
 
-    public GeneralServiceException(String message, Throwable cause) {
-      super(message, cause);
-    }
+  public GeneralServiceException(String message, Throwable cause) {
+    super(message, cause);
+  }
 
-    public GeneralServiceException(String message) {
-      super(message);
-    }
+  public GeneralServiceException(String message) {
+    super(message);
+  }
 
-    public GeneralServiceException(Throwable cause) {
-      super(cause);
-    }
+  public GeneralServiceException(Throwable cause) {
+    super(cause);
+  }
 
 }
