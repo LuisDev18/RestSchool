fd9984573335af284630b537077e9b93a9ec7f9d
Heraud Mendoza Huaraca <u18302063@utp.edu.pe>
2023-07-23 22:28:55 -0500

    Agregando los cambios del servicio Alumno

--- alumno-service/src/main/java/com/example/alumnoservice/service/impl/AlumnoServiceImpl.java
+++ alumno-service/src/main/java/com/example/alumnoservice/service/impl/AlumnoServiceImpl.java
@@ -29,4 +29,77 @@ public class AlumnoServiceImpl implements AlumnoService {
       throw e;
     }
         }
+
+    @Override
+    public Alumno findById(int id) {
+        Alumno registro =
+                repository
+                        .findById(id)
+                        .orElseThrow(
+                                () -> new ResourceNotFoundException(
+                                        "Alumno", "idAlumno", Long.valueOf(id)));
+        return registro;
+    }
+
+    @Override
+    public Alumno save(Alumno alumno) {
+        Alumno registro = repository.save(alumno);
+        return registro;
+    }
+
+    @Override
+    public Alumno update(Alumno alumno) {
+        try {
+            Alumno alumnoU =
+                    repository
+                            .findById(alumno.getId())
+                            .orElseThrow(
+                                    () ->
+                                            new ResourceNotFoundException(
+                                                    "Alumno",
+                                                    "idAlumno",
+                                                    Long.valueOf(alumno.getId())));
+
+            alumnoU.setDni(alumno.getDni());
+            alumnoU.setCelular(alumno.getCelular());
+            alumnoU.setEmail(alumno.getEmail());
+            alumnoU.setApellidoPaterno(alumno.getApellidoPaterno());
+            alumnoU.setNombre(alumno.getNombre());
+            alumnoU.setApellidoMaterno(alumno.getApellidoMaterno());
+            alumnoU.setGenero(alumno.getGenero());
+            alumnoU.setDireccion(alumno.getDireccion());
+
+
+            Alumno registro = repository.save(alumnoU);
+            return registro;
+        } catch (ResourceNotFoundException e) {
+            log.info(e.getMessage());
+
+            throw e;
+        } catch (Exception e) {
+            log.error(e.getMessage());
+            throw e;
+        }
+    }
+
+    @Override
+    public void delete(int id) {
+        try {
+            Alumno alumno =
+                    repository
+                            .findById(id)
+                            .orElseThrow(
+                                    () ->
+                                            new ResourceNotFoundException(
+                                                    "Alumno", "idAlumno", Long.valueOf(id)));
+            repository.delete(alumno);
+        } catch (ResourceNotFoundException e) {
+            log.info(e.getMessage());
+
+            throw e;
+        } catch (Exception e) {
+            log.error(e.getMessage());
+            throw e;
+        }
+    }
 }
